9dfbe4614ff3470b3eb0fd350ed17fb4626b9105
style77 <stylek777@gmail.com>
2023-12-17 22:42:55 +0100

    refactor: optimize icon component

--- src/utils/icon.tsx
+++ src/utils/icon.tsx
@@ -1,51 +1,90 @@
 import "./general.scss";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import * as FaIcons from "@fortawesome/free-solid-svg-icons";
+import * as AllIcons from "./icons";
 
 type IconProps = {
     src?: string;
 
     onClick?: any;
     active?: boolean;
-    
+    open?: boolean;
+
     className?: string;
-    ui?: boolean;
+    ui?: boolean;  // Use UI icons (from icon/ui)
+    fafa?: string;  // FontAwesome Icon
+    allIcons?: boolean;  // Use all icons (from icon)
+
+    invert?: boolean;
 
     width?: number;
     height?: number;
 }
 
 export const Icon = (props: IconProps) => {
-    var src = `icon/${props.ui != null ? "ui/" : ""}${props.src}.png`;
-
-    var prtclk = "";
-    if (props.src) {
-        if (props.onClick != null) {
-            prtclk = "prtclk";
-        }
-    }
-
-    return (
-        <div
-            className={`uicon ${props.className || ""} ${prtclk}`}
-            data-active={props.active}
+    if (props.fafa) {
+        return (
+            <div
+                className={`uicon prtclk ${props.className || ""}`}
+                onClick={props.onClick}
+            >
+                <FontAwesomeIcon
+                    style={{
+                        width: props.width,
+                        height: props.height || props.width,
+                    }}
+                    data-invert={props.invert}
+                    icon={
+                        (FaIcons as any)[props.fafa]
+                    }
+                />
+            </div>
+        );
+    } else if (props.allIcons && props.src) {
+        var CustomIcon = (AllIcons as any)[props.src];
+        return (
+          <div
+            className={`uicon prtclk ${props.className || ""}`}
             onClick={props.onClick}
-        >
-            {props.className == "tsIcon" ? (
-                <div
-                    style={{ width: props.width, height: props.width }}
-                >
+          >
+            <CustomIcon
+              style={{
+                width: props.width,
+                height: props.height || props.width,
+              }}
+            />
+          </div>
+        );
+      } else {
+        var src = `icon/${props.ui ? "ui/" : ""}${props.src}.png`;
+        return (
+            <div
+                className={`uicon ${props.className || ""} prtclk`}
+                data-active={props.active}
+                data-open={props.open}
+                onClick={props.onClick}
+            >
+                {props.className == "tsIcon" ? (
+                    <div
+                        style={{ width: props.width, height: props.width }}
+                        data-invert={props.invert}
+                    >
+                        <img
+                            width={props.width}
+                            height={props.height}
+                            src={src}
+                            data-invert={props.invert}
+                        />
+                    </div>
+                ) : (
                     <img
                         width={props.width}
                         height={props.height}
                         src={src}
+                        data-invert={props.invert}
                     />
-                </div>
-            ) : (
-                <img
-                    width={props.width}
-                    height={props.height}
-                    src={src}
-                />
-            )}
-        </div>
-    );
+                )}
+            </div>
+        );
+    }
 };
