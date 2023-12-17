300984f3bebf0da562a5f8e730aa6324c06e309e
style77 <stylek777@gmail.com>
2023-12-18 00:20:57 +0100

    refactor: move sidepane reducer to switches

--- src/reducers/sidepaneReducer.ts
+++ src/reducers/sidepaneReducer.ts
@@ -52,68 +52,69 @@ const initialState = {
 
 
 export default function sidepaneReducer(state = initialState, action) {
-    if (action.type === "TOGGLE_QUICKS") {
-        let updatedQuicks = state.quicks;
-        let updatedBrightness = state.brightness;
+    switch (action.type) {
+        case "TOGGLE_QUICKS": {
+            let updatedQuicks = state.quicks;
+            let updatedBrightness = state.brightness;
 
-        if (action.payload === "Airplane Mode") {
-            updatedQuicks = state.quicks.map(quick => {
-                if (quick.name === "WiFi" || quick.name === "Bluetooth") {
-                    return { ...quick, state: false };
-                } else if (quick.name === "Airplane Mode") {
-                    return { ...quick, state: !quick.state };
-                }
-                return quick;
-            });
-        } else if (action.payload === "WiFi" || action.payload === "Bluetooth") {
-            updatedQuicks = state.quicks.map(quick => {
-                if (quick.name === "Airplane Mode") {
-                    return { ...quick, state: false };
-                } else if (quick.name === action.payload) {
-                    return { ...quick, state: !quick.state };
-                }
-                return quick;
-            });
-        } else if (action.payload === "Battery Saver") {
-            updatedQuicks = state.quicks.map(quick => {
-                if (quick.name === "Battery Saver") {
-                    const newState = !quick.state;
-                    updatedBrightness = newState ? 70 : 100;
-                    return { ...quick, state: newState };
-                }
-                return quick;
-            });
+            if (action.payload === "Airplane Mode") {
+                updatedQuicks = state.quicks.map(quick => {
+                    if (quick.name === "WiFi" || quick.name === "Bluetooth") {
+                        return { ...quick, state: false };
+                    } else if (quick.name === "Airplane Mode") {
+                        return { ...quick, state: !quick.state };
+                    }
+                    return quick;
+                });
+            } else if (action.payload === "WiFi" || action.payload === "Bluetooth") {
+                updatedQuicks = state.quicks.map(quick => {
+                    if (quick.name === "Airplane Mode") {
+                        return { ...quick, state: false };
+                    } else if (quick.name === action.payload) {
+                        return { ...quick, state: !quick.state };
+                    }
+                    return quick;
+                });
+            } else if (action.payload === "Battery Saver") {
+                updatedQuicks = state.quicks.map(quick => {
+                    if (quick.name === "Battery Saver") {
+                        const newState = !quick.state;
+                        updatedBrightness = newState ? 70 : 100;
+                        return { ...quick, state: newState };
+                    }
+                    return quick;
+                });
 
-            return { ...state, quicks: updatedQuicks, brightness: updatedBrightness };
-        } else if (action.payload === "Theme") {
-            updatedQuicks = state.quicks.map(quick => {
-                if (quick.name === "Theme") {
-
-                    document.body.dataset.theme = quick.src === "moon" ? "light" : "dark";
-                    return { ...quick, state: !quick.state, src: quick.src === "moon" ? "sun" : "moon" };
-                }
-                return quick;
-            });
-        } else {
-            updatedQuicks = state.quicks.map(quick => {
-                if (quick.name === action.payload) {
-                    return { ...quick, state: !quick.state };
-                }
-                return quick;
-            });
+                return { ...state, quicks: updatedQuicks, brightness: updatedBrightness };
+            } else if (action.payload === "Theme") {
+                updatedQuicks = state.quicks.map(quick => {
+                    if (quick.name === "Theme") {
+                        document.body.dataset.theme = quick.src === "moon" ? "light" : "dark";
+                        return { ...quick, state: !quick.state, src: quick.src === "moon" ? "sun" : "moon" };
+                    }
+                    return quick;
+                });
+            } else {
+                updatedQuicks = state.quicks.map(quick => {
+                    if (quick.name === action.payload) {
+                        return { ...quick, state: !quick.state };
+                    }
+                    return quick;
+                });
+            }
+            return { ...state, quicks: updatedQuicks };
         }
-        return { ...state, quicks: updatedQuicks };
-    } else if (action.type == "SET_BRIGHTNESS") {
-        return { ...state, brightness: action.payload };
-    } else if (action.type == "TOGGLE_SIDEPANE") {
-        return { ...state, calendarHide: true, hide: !state.hide };
-    } else if (action.type == "HIDE_SIDEPANE") {
-        return { ...state, hide: true };
-    } else if (action.type == "TOGGLE_CALENDAR") {
-        return { ...state, calendarHide: !state.calendarHide, hide: true };
-    } else if (action.type == "HIDE_CALENDAR") {
-        return { ...state, calendarHide: true };
-    } else {
-        return state;
+        case "SET_BRIGHTNESS":
+            return { ...state, brightness: action.payload };
+        case "TOGGLE_SIDEPANE":
+            return { ...state, calendarHide: true, hide: !state.hide };
+        case "HIDE_SIDEPANE":
+            return { ...state, hide: true };
+        case "TOGGLE_CALENDAR":
+            return { ...state, calendarHide: !state.calendarHide, hide: true };
+        case "HIDE_CALENDAR":
+            return { ...state, calendarHide: true };
+        default:
+            return state;
     }
-};
+}
