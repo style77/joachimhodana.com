36cea32e1e6293bd5122b98621989a25f67fe38b
style77 <stylek777@gmail.com>
2023-12-17 22:39:58 +0100

    chore: applicationsReducer

--- src/reducers/applicationsReducer.ts
+++ src/reducers/applicationsReducer.ts
@@ -1,17 +1,61 @@
+import { apps } from "../utils/defaults";
 
-const initialState = {
+type Window = {
+    title: string;
+    position: {
+        x: number;
+        y: number;
+    };
+    size: {
+        width: number;
+        height: number;
+    };
+}
+
+type Application = {
+    id: string;
+    window: Window;
+};
+
+
+const initialState: {
+    applications: Application[];
+    activeApplication: string | null;
+} = {
     applications: [],
     activeApplication: null,
 };
 
-export default function applicationsReducer(state = initialState, action) {
-    switch (action.type) {
-        case 'SET_APPLICATIONS':
-            return {
-                ...state,
-                applications: action.payload,
-            };
+type Action = {
+    type: string;
+    payload: any;
+};
+
+export default function applicationsReducer(state = initialState, action: Action) {
+    switch (action.type) { 
         case 'SET_ACTIVE_APPLICATION':
+            if (!state.applications.find((application) => application.id === action.payload)) {
+                const appDefaults: Application = {
+                    id: action.payload,
+                    window: {
+                        title: apps[action.payload].window.title,
+                        position: {
+                            x: 0,
+                            y: 0,
+                        },
+                        size: {
+                            width: 240,
+                            height: 240,
+                        },
+                    },
+                }
+                return {
+                    ...state,
+                    applications: [...state.applications, appDefaults],
+                    activeApplication: action.payload,
+                };
+            }
+
             return {
                 ...state,
                 activeApplication: action.payload,
