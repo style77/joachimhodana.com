adfec14f1d3429b46b8034cbdbe60b792c388031
style77 <stylek777@gmail.com>
2023-12-17 22:41:45 +0100

    chore: toggle quicks action in  sidepane reducer

--- src/reducers/sidepaneReducer.ts
+++ src/reducers/sidepaneReducer.ts
@@ -4,67 +4,114 @@ const initialState = {
             ui: true,
             src: "wifi",
             name: "WiFi",
-            state: "network.wifi.state",
-            action: "STNGTOGG",
+            state: true,
         },
         {
             ui: true,
             src: "bluetooth",
             name: "Bluetooth",
-            state: "devices.bluetooth",
-            action: "STNGTOGG",
+            state: true,
         },
         {
             ui: true,
             src: "airplane",
             name: "Flight Mode",
-            state: "network.airplane",
+            state: false,
             action: "flightMode",
         },
         {
             ui: true,
             src: "saver",
             name: "Battery Saver",
-            state: "system.power.saver.state",
-            action: "STNGTOGG",
+            state: false
         },
         {
             ui: true,
             src: "sun",
             name: "Theme",
-            state: "person.theme",
+            state: false,
             action: "changeTheme",
         },
         {
             ui: true,
             src: "nightlight",
             name: "Night Light",
-            state: "system.display.nightlight.state",
-            action: "STNGTOGG",
+            state: false,
         },
     ],
     hide: true,
     banhide: true,
     calhide: true,
+
+    brightness: 100,
 };
 
 
 export default function sidepaneReducer(state = initialState, action) {
-    if (action.type == "PANETHEM") {
-        var tmpState = { ...state };
-        tmpState.quicks[4].src = action.payload;
-        return tmpState;
-    } else if (action.type == "BANDTOGG") {
+    if (action.type === "TOGGLE_QUICKS") {
+        let updatedQuicks = state.quicks;
+        let updatedBrightness = state.brightness;
+
+        if (action.payload === "Flight Mode") {
+            updatedQuicks = state.quicks.map(quick => {
+                if (quick.name === "WiFi" || quick.name === "Bluetooth") {
+                    return { ...quick, state: false };
+                } else if (quick.name === "Flight Mode") {
+                    return { ...quick, state: !quick.state };
+                }
+                return quick;
+            });
+        } else if (action.payload === "WiFi" || action.payload === "Bluetooth") {
+            updatedQuicks = state.quicks.map(quick => {
+                if (quick.name === "Flight Mode") {
+                    return { ...quick, state: false };
+                } else if (quick.name === action.payload) {
+                    return { ...quick, state: !quick.state };
+                }
+                return quick;
+            });
+        } else if (action.payload === "Battery Saver") {
+            updatedQuicks = state.quicks.map(quick => {
+                if (quick.name === "Battery Saver") {
+                    const newState = !quick.state;
+                    updatedBrightness = newState ? 70 : 100;
+                    return { ...quick, state: newState };
+                }
+                return quick;
+            });
+
+            return { ...state, quicks: updatedQuicks, brightness: updatedBrightness };
+        } else if (action.payload === "Theme") {
+            updatedQuicks = state.quicks.map(quick => {
+                if (quick.name === "Theme") {
+
+                    document.body.dataset.theme = quick.src === "moon" ? "light" : "dark";
+                    return { ...quick, state: !quick.state, src: quick.src === "moon" ? "sun" : "moon" };
+                }
+                return quick;
+            });
+        } else {
+            updatedQuicks = state.quicks.map(quick => {
+                if (quick.name === action.payload) {
+                    return { ...quick, state: !quick.state };
+                }
+                return quick;
+            });
+        }
+        return { ...state, quicks: updatedQuicks };
+    } else if (action.type == "SET_BRIGHTNESS") {
+        return { ...state, brightness: action.payload };
+    } else if (action.type == "TOGGLE_BAND") {
         return { ...state, banhide: !state.banhide };
-    } else if (action.type == "BANDHIDE") {
+    } else if (action.type == "HIDE_BAND") {
         return { ...state, banhide: true };
-    } else if (action.type == "PANETOGG") {
+    } else if (action.type == "TOGGLE_SIDEPANE") {
         return { ...state, hide: !state.hide };
-    } else if (action.type == "PANEHIDE") {
+    } else if (action.type == "HIDE_SIDEPANE") {
         return { ...state, hide: true };
-    } else if (action.type == "CALNTOGG") {
+    } else if (action.type == "TOOGLE_CAL") {
         return { ...state, calhide: !state.calhide };
-    } else if (action.type == "CALNHIDE") {
+    } else if (action.type == "HIDE_CAL") {
         return { ...state, calhide: true };
     } else {
         return state;
