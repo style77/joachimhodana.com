ca1aebdd23451a9884d45a971199863ab634b27e
style77 <stylek777@gmail.com>
2023-12-18 03:55:41 +0100

    chore: add selection box to desktop

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { MouseEventHandler } from "react";
+import { MouseEventHandler, useEffect, useState } from "react";
 import { ErrorBoundary } from "react-error-boundary";
 import Background from "./containers/background/background";
 import Taskbar from "./components/taskbar";
@@ -53,12 +53,71 @@ function ErrorFallback({ error, resetErrorBoundary }: { error: Error, resetError
 }
 
 function App() {
+  const [selecting, setSelecting] = useState(false);
+  const [selectionBox, setSelectionBox] = useState({});
+
+  useEffect(() => {
+    const handleMouseUpDocument = () => {
+      if (selecting) {
+        setSelecting(false);
+      }
+    };
+
+    const handleMouseMoveDocument = (e) => {
+      if (selecting) {
+        const { clientX, clientY } = e;
+        setSelectionBox((prevBox) => ({
+          ...prevBox,
+          endX: clientX,
+          endY: clientY,
+        }));
+      }
+    };
+
+    document.addEventListener('mouseup', handleMouseUpDocument);
+    document.addEventListener('mousemove', handleMouseMoveDocument);
+
+    return () => {
+      document.removeEventListener('mouseup', handleMouseUpDocument);
+      document.removeEventListener('mousemove', handleMouseMoveDocument);
+    };
+  }, [selecting]);
+
+  const handleMouseDown = (e) => {
+    setSelecting(true);
+    const { clientX, clientY } = e;
+    setSelectionBox({
+      startX: clientX,
+      startY: clientY,
+      endX: clientX,
+      endY: clientY,
+    });
+  };
+
+  const handleMouseUp = () => {
+    setSelecting(false);
+  };
+
   return (
     <div className="App">
       <ErrorBoundary FallbackComponent={ErrorFallback}>
         <div className="appwrap">
           <Background />
-          <div className="desktop" data-menu="desk">
+          <div className="desktop" data-menu="desk"
+            onMouseDown={handleMouseDown}
+            onMouseUp={handleMouseUp}
+          >
+            {selecting && (
+              <div
+                className="selectionBox"
+                style={{
+                  left: Math.min(selectionBox.startX, selectionBox.endX),
+                  top: Math.min(selectionBox.startY, selectionBox.endY),
+                  width: Math.abs(selectionBox.endX - selectionBox.startX),
+                  height: Math.abs(selectionBox.endY - selectionBox.startY),
+                }}
+              />
+            )}
             <Desktop />
             <SidePane />
             <Calendar />
