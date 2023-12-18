78bcd7fb2df47e782215353155fcf1c167f19372
style77 <stylek777@gmail.com>
2023-12-18 17:49:33 +0100

    chore: lazy component

---
+++ src/utils/lazy.tsx
@@ -0,0 +1,14 @@
+import { useState, useEffect } from "react";
+
+export const LazyComponent = ({ show, children }: {
+    show: boolean;
+    children: React.ReactNode;
+}) => {
+    const [loaded, setLoad] = useState(false);
+
+    useEffect(() => {
+        if (show && !loaded) setLoad(true);
+    }, [show]);
+
+    return show || loaded ? <>{children}</> : null;
+};
