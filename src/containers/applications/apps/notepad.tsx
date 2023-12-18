5899351eaf0640f6ecc3a3e8c4509760425ca59c
style77 <stylek777@gmail.com>
2023-12-18 20:23:15 +0100

    chore: notepad component

---
+++ src/containers/applications/apps/notepad.tsx
@@ -0,0 +1,41 @@
+import React from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { ToolBar } from "../toolbar";
+
+export const Notepad = () => {
+    const applicationsState = useSelector((state) => state.applications);
+    const wnapp = applicationsState.applications.find((app) => app.id === "Notepad");
+
+    return (
+        <div
+            className="notepad floatTab dpShad"
+            data-size={wnapp.window.size}
+            data-max={wnapp.window.max}
+            style={{
+                ...(wnapp.window.size == "cstm" ? wnapp.window.dim : null),
+                zIndex: wnapp.window.z,
+            }}
+            data-hide={wnapp.hide}
+            id={wnapp.icon + "App"}
+        >
+            <ToolBar
+                app={wnapp}
+                icon={wnapp.icon}
+                size={wnapp.window.size}
+                name={wnapp.window.title || "Untitled - Notepad"}
+            />
+            <div className="windowScreen flex flex-col" data-dock="true">
+                <div className="flex text-xs py-2 topBar">
+                    <div className="mx-2">File</div>
+                    <div className="mx-4">Edit</div>
+                    <div className="mx-4">View</div>
+                </div>
+                <div className="restWindow h-full flex-grow">
+                    <div className="w-full h-full overflow-hidden">
+                        <textarea className="noteText win11Scroll" id="textpad" defaultValue={wnapp.content || ""} />
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
--- src/containers/applications/index.ts
+++ src/containers/applications/index.ts
@@ -3,4 +3,5 @@ import "./tabs2.scss";
 import "./wnapp.scss";
 
 export * from './apps/browser'
+export * from './apps/notepad'
 export * from './apps/visualstudiocode'
