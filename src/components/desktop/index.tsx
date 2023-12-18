9bc4a74fc8c65a79489aab08b188095818339bef
style77 <stylek777@gmail.com>
2023-12-18 03:55:56 +0100

    chore: on click set active app

--- src/components/desktop/index.tsx
+++ src/components/desktop/index.tsx
@@ -1,28 +1,37 @@
-import { useSelector } from "react-redux"
+import { useDispatch, useSelector } from "react-redux"
 import { Icon } from "../../utils/icon";
 
 import "../search/searchpane.scss";
 import "../sidepane/sidepane.scss";
 import "../start/startmenu.scss";
+import "./desktop.scss";
 
 export const Desktop = () => {
     const desktopState = useSelector((state: any) => state.desktop)
 
+    const dispatch = useDispatch()
+
     return (
         <div className="desktopCont">
-          {desktopState.applications.map((app, i) => {
-              return (
-                <div key={i} className="dskApp" tabIndex={i}>
-                  <Icon
-                    clicked={true}
-                    className="dskIcon"
-                    src={app.icon}
-                    width={36}
-                  />
-                  <div className="appName">{app.name}</div>
-                </div>
-              );
+            {desktopState.applications.map((app, i) => {
+                return (
+                    <div key={i} className="dskApp" tabIndex={i}>
+                        <Icon
+                            clicked={true}
+                            className="dskIcon"
+                            src={app.icon}
+                            width={36}
+                            onClick={() => {
+                                dispatch({
+                                    type: "SET_ACTIVE_APPLICATION",
+                                    payload: app.id,
+                                })
+                            }}
+                        />
+                        <div className="appName">{app.name}</div>
+                    </div>
+                );
             })}
         </div>
-      );
+    );
 }
