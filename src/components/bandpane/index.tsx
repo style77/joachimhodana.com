831802211904f3a94cbac98217bddc97fcc6e310
style77 <stylek777@gmail.com>
2023-12-21 22:14:29 +0100

    chore: open mullvad on bandpane icon click

--- src/components/bandpane/index.tsx
+++ src/components/bandpane/index.tsx
@@ -1,40 +1,52 @@
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { Icon } from "../../utils/icon";
 import { RootState } from "../../reducers";
 
 export const BandPane = () => {
-    const sidepane = useSelector((state: RootState) => state.sidepane);
-  
-    return (
-      <div
-        className="bandpane dpShad"
-        data-hide={sidepane.bandhide}
-        // @ts-expect-error Variable is not typed
-        style={{ "--prefix": "BAND" }}
-      >
-        <div className="bandContainer">
-          <Icon
-            className="hvlight"
-            width={14}
-            clicked={true}
-            ui
-            src="mullvad_locked"
-          />
-          <Icon
-            className="hvlight"
-            width={17}
-            clicked={true}
-            ui
-            src="docker"
-          />
-          <Icon
-            className="hvlight"
-            width={14}
-            clicked={true}
-            ui
-            src="discord"
-          />
-        </div>
+  const sidepane = useSelector((state: RootState) => state.sidepane);
+
+  const dispatch = useDispatch();
+
+  return (
+    <div
+      className="bandpane dpShad"
+      data-hide={sidepane.bandhide}
+      // @ts-expect-error Variable is not typed
+      style={{ "--prefix": "BAND" }}
+    >
+      <div className="bandContainer">
+        <Icon
+          className="hvlight"
+          width={14}
+          clicked={true}
+          ui
+          src="mullvad_locked"
+          onClick={() => {
+            dispatch({
+              type: "SET_ACTIVE_APPLICATION",
+              payload: {
+                id: "Mullvad",
+                type: "app",
+                name: "Mullvad"
+              },
+            })
+          }}
+        />
+        <Icon
+          className="hvlight"
+          width={17}
+          clicked={true}
+          ui
+          src="docker"
+        />
+        <Icon
+          className="hvlight"
+          width={14}
+          clicked={true}
+          ui
+          src="discord"
+        />
       </div>
-    );
-  };
+    </div>
+  );
+};
