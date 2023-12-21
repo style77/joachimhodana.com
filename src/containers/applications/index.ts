ce5f4b4bbb1647ffe943ea46eff6a176b8c75645
style77 <stylek777@gmail.com>
2023-12-21 21:12:14 +0100

    chore: add calculator app to index

--- src/containers/applications/index.ts
+++ src/containers/applications/index.ts
@@ -5,4 +5,5 @@ import "./wnapp.scss";
 export * from './apps/browser'
 export * from './apps/notepad'
 export * from './apps/github'
+export * from './apps/calculator'
 export * from './apps/visualstudiocode'
