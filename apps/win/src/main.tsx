f82eaf7e220135b4110a43fbee7980ea53b2315c
style77 <stylek777@gmail.com>
2024-10-15 02:25:07 +0200

    chore: add vercel's analytics to win app

--- apps/win/src/main.tsx
+++ apps/win/src/main.tsx
@@ -5,12 +5,15 @@ import { Provider } from 'react-redux';
 import './index.css'
 import store from './reducers/index.js';
 
+import { Analytics } from "@vercel/analytics/react"
+
 const root = ReactDOM.createRoot(document.getElementById('root')!)
 
 root.render(
   <React.StrictMode>
     <Provider store={store}>
       <App />
+      <Analytics />
     </Provider>
   </React.StrictMode>,
 )
