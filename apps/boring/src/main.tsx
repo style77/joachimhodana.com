b46eec6ccd7e6815434befcac4df302a9c34337f
style77 <stylek777@gmail.com>
2024-10-17 23:23:36 +0200

    refactor: migrate to turborepo

--- apps/boring/src/main.tsx
+++ apps/boring/src/main.tsx
@@ -1,16 +1,10 @@
-import { StrictMode } from 'react';
-import * as ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import App from './App.tsx'
+import './index.css'
 
-import App from './app/app';
-
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
-root.render(
+createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </StrictMode>
-);
+    <App />
+  </StrictMode>,
+)
