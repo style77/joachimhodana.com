667756d58a1d66676663fa4d51b2f305165daea2
style77 <stylek777@gmail.com>
2024-10-18 12:15:49 +0200

    fix: pagespeed insights improvements

--- apps/boring/src/components/Header.tsx
+++ apps/boring/src/components/Header.tsx
@@ -4,7 +4,7 @@ export default function Header() {
     return (
         <div className="flex flex-col items-center justify-center w-full mt-14 gap-2">
             <img className="rounded-full border-2 border-gray-700 drop-shadow-[0_0_30px_rgba(255,255,255,0.05)]" src="https://avatars.githubusercontent.com/u/36776006?v=4" alt="avatar" width={150} height={150} />
-            <div className="flex flex-col items-center gap-2 py-4 font-bold text-3xl md:text-4xl lg:text-5xl text-center" style={{lineHeight: 1.2}}>
+            <div className="flex flex-col items-center gap-2 py-4 font-bold text-3xl md:text-4xl lg:text-5xl text-center" style={{ lineHeight: 1.2 }}>
                 <h1 className="">Hey, I'm{" "}
                     <p className="bg-gradient-to-r from-red-500 to-red-600 inline-block text-transparent bg-clip-text drop-shadow-[0_0_30px_rgba(255,255,255,0.35)]">Joachim</p>
                 </h1>
@@ -13,9 +13,15 @@ export default function Header() {
                 </h3>
                 <p className="text-base lg:text-lg w-fit font-light text-gray-400 mt-3">Software Developer, Fullstack Developer & Data Engineer | Computer Science Student @ PJATK</p>
             </div>
-            <a className="btn border-red-700 bg-transparent hover:border-red-500 drop-shadow-[0_0_30px_rgba(255,255,255,0.25)]" href="https://calendly.com/hodanajoachim/30min">
-                <span className="bg-gradient-to-r from-red-500 to-red-600 inline-block text-transparent bg-clip-text drop-shadow-[0_0_30px_rgba(255,255,255,0.25)]">Book Short Call</span>
-                <IoOpenOutline className="text-red-500" size={20} />
+            <a
+                className="flex items-center btn border border-red-700 bg-transparent hover:border-red-500 transition-all duration-300 drop-shadow-lg"
+                href="https://calendly.com/hodanajoachim/30min"
+                aria-label="Book a short call"
+            >
+                <span className="bg-gradient-to-r from-red-500 to-red-600 inline-block text-transparent bg-clip-text drop-shadow-lg">
+                    Book Short Call
+                </span>
+                <IoOpenOutline className="text-red-500 ml-2" size={20} />
             </a>
         </div>
     );
--- apps/boring/src/index.page.tsx
+++ apps/boring/src/index.page.tsx
@@ -25,7 +25,7 @@ function Page() {
         </div>
         <div className="flex flex-col w-full items-center">
           <div className="relative h-px w-8/12 bg-gray-700">
-            <a className="absolute bottom-[-15px] left-1/2 transform -translate-x-1/2 rotate-[-12deg] py-2 px-4 rounded-full bg-red-800 text-gray-300 drop-shadow-[0_0_30px_rgba(255,255,255,0.25)]">Services</a>
+            <span className="absolute bottom-[-15px] left-1/2 transform -translate-x-1/2 rotate-[-12deg] py-2 px-4 rounded-full bg-red-800 text-gray-300 drop-shadow-[0_0_30px_rgba(255,255,255,0.25)]">Services</span>
           </div>
           <Cards />
           <div className="w-full bg-gray-700/25 border-y border-gray-700">
